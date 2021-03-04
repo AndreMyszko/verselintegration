3b3996a15d8e024a4876bc1d0ecf61549d44508a
ANDRÉ MYSZKO <andre.myszko@gmail.com>
2021-03-03 22:09:51 -0300

    primeira pagina

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+//import React from 'react';
+import {useState} from 'react';
+
+function Home(){
+    return (
+        //Carregamento do HTML da página está sendo feito aqui.
+        <div>
+            <h1>Home</h1>
+            <Contador />
+            <p>Test</p>
+        </div>
+        
+    )
+}
+
+function Contador(){
+    const [contador, setContador] = useState(1);
+
+    function adicionarContador(){
+        setContador(contador + 1);
+    }
+
+    return(
+        //Carregamento do HTML da página está sendo feito aqui.
+        <div>
+            <div>{contador}</div>
+            <button onClick={adicionarContador}>Adicionar</button>
+        </div>
+    )
+}
+
+export default Home;
