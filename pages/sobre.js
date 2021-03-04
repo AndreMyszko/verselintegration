c07048744f9ea689fc50e9eae2e9f9bd259eba63
ANDRÉ MYSZKO <andre.myszko@gmail.com>
2021-03-03 23:30:11 -0300

    adicionando pagina sobre

---
+++ pages/sobre.js
@@ -0,0 +1,15 @@
+function Sobre(){
+    return (
+        //Carregamento do HTML da página está sendo feito a
+        <div>
+            <h1>Sobre</h1>
+            <Contador />
+            <p>Test</p>
+            <a href="/home">Acessar página *HOME*</a>
+        </div>
+        
+    )
+}
+
+
+export default Sobre;
