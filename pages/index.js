ec0320148ab4c2c9a5ab5c738625fcdaccf063d9
ANDRÉ MYSZKO <andre.myszko@gmail.com>
2021-03-03 23:36:25 -0300

    update sobre

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,33 @@
 //import React from 'react';
-import {useState} from 'react';
+// import {useState} from 'react';
 
 function Home(){
     return (
         //Carregamento do HTML da página está sendo feito aqui.
         <div>
             <h1>Home Update</h1>
-            <Contador />
-            <p>Test</p>
+            {/* <Contador />
+            <p>Test</p> */}
             <a href="/sobre">Acessar página *SOBRE*</a>
         </div>
         
     )
 }
 
-function Contador(){
-    const [contador, setContador] = useState(1);
+// function Contador(){
+//     const [contador, setContador] = useState(1);
 
-    function adicionarContador(){
-        setContador(contador + 1);
-    }
+//     function adicionarContador(){
+//         setContador(contador + 1);
+//     }
 
-    return(
-        //Carregamento do HTML da página está sendo feito aqui.
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )
-}
+//     return(
+//         //Carregamento do HTML da página está sendo feito aqui.
+//         <div>
+//             <div>{contador}</div>
+//             <button onClick={adicionarContador}>Adicionar</button>
+//         </div>
+//     )
+// }
 
 export default Home;
--- pages/sobre.js
+++ pages/sobre.js
@@ -3,13 +3,10 @@ function Sobre(){
         //Carregamento do HTML da página está sendo feito a
         <div>
             <h1>Sobre</h1>
-            <Contador />
-            <p>Test</p>
             <a href="/">Acessar página *HOME*</a>
         </div>
         
     )
 }
-
 
 export default Sobre;
